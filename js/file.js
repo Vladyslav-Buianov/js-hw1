9d49704fe18e3b4fcc6c799c7f6cfaa98d436406
Vladyslav <vitbuianov.20130225@gmail.com>
2026-01-02 12:34:04 +0100

    hw1.1

---
+++ js/file.js
@@ -0,0 +1,13 @@
+const age = 12;
+console.log(age);
+const name = "Vladyslav";
+console.log(name);
+const isStudent = true;
+console.log(isStudent);
+const myString = "Креативні люди правлять світом"
+console.log(myString);
+let myNumber = 57;
+myNumber += 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
